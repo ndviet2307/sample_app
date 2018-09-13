f0603324df7e6fe3dd2b7a959a15095c76cd3434
Nguyen Duc Viet <Ndviet2307@gmail.com>
2018-09-13 09:59:02 +0700

    Chapter 12: Password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,11 @@
 class UserMailer < ApplicationMailer
-
   def account_activation user
     @user = user
     mail to: user.email, subject: t(".subject")
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t(".subject")
   end
 end
